59ba758798965d8d8017c3c16b14f4abbe5c884b
vaczeen <Metis_guntav@hotmail.com>
2023-02-24 16:02:02 +0700

    regi

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,11 +1,55 @@
 const User = require('../models/user')
-const express = require('express')
-const mongoose = require('mongoose')
+const bcrypy = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+const register = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        if (!(username && password)) {
+            return res.status(400).json({ "detail": "All input is required" });
+        }
+        //check if user already exist
+        //Validate if user exist in our database 
+        const oldUser = await User.findOne({ username: username })
+        if (oldUser) {
+            return res.status(400).json({ "detail": "User Already Exist. Please Login" })
+        }
+        //Encrypt user password
+        let encryptPassword = bcrypt.hashsync(password, 10)
+
+        //Create user in our database
+        try {
+            let data = req.body
+            delete data._id
+            await User.create({
+                ...data,
+                password: encryptPassword
+            }, (err, result) => {
+                if (err) res.status(400).send(err)
+                else {
+                    res.status(201).send(result)
+                }
+            })
+        } catch (error) {
+            res.status(400).json({ message: 'Bad request' })
+        }
+    } catch (error) {
+        res.status(500).json({ detail: error })
+
+    }
+}
+
+const login = (res, req) => {
+    try {
+        const { username, password } = req.body
+        
+    } catch (error) {
+        res.status(500).json({ detail: error })
+    }
+}
 
 const create = async (req, res) => {
     try {
-        console.log(req.body);
         const users = await User.create(req.body)
         res.status(201).send(users)
     } catch (error) {
@@ -26,7 +70,7 @@ const retrieve = async (req, res) => {
 
 const update = async (req, res) => {
     try {
-        const users = await User.findByIdAndUpdate(req.params.id, {...req.body})
+        const users = await User.findByIdAndUpdate(req.params.id, { ...req.body })
         res.status(200).send(users)
     } catch (error) {
         res.status(400).json({ message: 'Error while update user.', detail: error })
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(express.json());
 app.use("/test",userRoutes)
 
 const PORT = process.env.PORT || 1111 ;
