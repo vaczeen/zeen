95dda57f985ee2bd64f89f0f2806fb13e33a5ae1
Metis <Metis_guntav@hotmail.com>
2023-03-06 20:58:51 +0700

    new

--- src/models/order.js
+++ src/models/order.js
@@ -11,7 +11,7 @@ const OrderSchema = new Schema({
         require: true
     },
     product: {
-        type: Schema.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'Product',
         reqiure: true
     },
